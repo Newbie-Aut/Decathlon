b9aacb61e678c934314d7637da78175df7252f73
Monika Johansson <monikajohansson@live.se>
2021-04-21 14:10:18 +0200

    Added given, when, then

--- DecathlonHeptathlon/src/test/java/stepDefinitions/StepDefinitions.java
+++ DecathlonHeptathlon/src/test/java/stepDefinitions/StepDefinitions.java
@@ -1,5 +1,45 @@
 package stepDefinitions;
 
+import java.util.Scanner;
+import java.lang.Math.*;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class StepDefinitions {
+	private String compName;
+	private int runningTime;
+	double finalResult;
+	double A = 25.4347;
+	double B = 18;
+	double C = 1.81;
+
+	Scanner sc = new Scanner(System.in);
+
+	@Given("I want to add a name1")
+	public void addingCompetitor() {
+
+		// System.out.println("Male or female competitor?");
+
+		System.out.println("Please enter the competitor's name:");
+		compName = sc.nextLine();
+	}
+
+	@When("I check the time in step")
+	public void checkResult() {
+		System.out.println("Please enter the result of the 100m run:");
+		runningTime = sc.nextInt();
+
+		int result = (int) (A * Math.pow((B - runningTime), C));
+
+		System.out.println(compName + "'s score is: " + result);
+
+	}
+
+	@Then("I verify the success in step")
+	public void verifyScore() {
+
+	}
 
 }
